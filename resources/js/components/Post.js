27888dca3e4e8a1a2a08dc9dddb109921471f6c6
locemarn <marcelon27@gmail.com>
2018-11-26 22:08:29 -0200

    partial to es6

---
+++ resources/js/components/Post.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Post extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentWillMount() {
+    let $this = this
+    
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <div className="card w-75">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" className="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+if(document.getElementById('post')) {
+  ReactDOM.render(<Post />, document.getElementById('post'))
+}
